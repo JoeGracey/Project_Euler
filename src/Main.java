055b96e42e855c71e39be300af33ba5b4bfe9e8c
Joe <joe.gracey@gmail.com>
2020-08-06 23:48:02 -0600

    multiples of 3 and 5

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,21 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        /*
+        create var = sum
+        increment to 1, 2, 3.. to 10
+        divided by 3 evenly
+        divided by 5 evenly
+        if divided evenly, add those numbers together
+
+         */
+        int sum = 0;
+
+        for(int i = 1; i < 1000; i++) {
+            if(i % 3 == 0 || i % 5 == 0) {
+                sum += i;
+                System.out.println("Sum: " + sum + " i: " + i);
+            }
+        }
     }
 }
