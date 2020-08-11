d4c308113df315172b0280642adf11e49804d3a1
Joe <joe.gracey@gmail.com>
2020-08-10 23:55:24 -0600

    LargestPrimeFactor

--- out/production/ProjectEuler/LargestPrimeFactor/src/Main.java
+++ out/production/ProjectEuler/LargestPrimeFactor/src/Main.java
@@ -2,26 +2,40 @@ public class Main {
 
     public static void main(String[] args) {
 	    /*
+	        Prime number is divisible by only 1 and itself
+
             The prime factors of 13195 are 5, 7, 13 and 29
 
             What is the largest prime factor of the number 600851475143
 
-            Loop through prime checking it with every number
+            Loop through num checking it with every number
             How do I check it?
             With a for loop?
             How many times do I loop through?
-            var for the number I'm checking
-            save and display a var for every prime number
+            create var for the num I'm checking
+            save and display a variable for every prime number
 	     */
 
+        long num = 600851475143L;
+        boolean flag = false;
 
-        long prime = 600851475143L;
-
-        for(long i = 0L; i < 10; i++) {
-            if(prime % i == 0) {
-                System.out.println(prime);
+        for(long i = 2L; i <= num / 2; ++i) {
+            if(num % i == 0) {
+                flag = true;
+                break;
             }
-            i++;
+        }
+
+        if(!flag) {
+            System.out.println(num);
+        } else {
+            System.out.println("X");
         }
     }
+
+    /*
+        I did not write this on my own. I looked it up and I'm trying to understand it.
+     */
 }
+
+
