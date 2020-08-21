c436a0113fbce007a456478b5c8c47eed318eef8
Joe <joe.gracey@gmail.com>
2020-08-20 23:39:13 -0600

    LargestPalindromeProduct

---
+++ LargestPalindromeProduct/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+	    // Palindromic number reads the same both ways
+        // largest two 2-digit number is 9009 = 91 * 99
+        // Find the largest two 3-digit numbers
+    }
+}
