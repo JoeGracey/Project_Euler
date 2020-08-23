7b894b14136fff13218cfdce69d49257e40cf298
Joe <joe.gracey@gmail.com>
2020-08-22 21:57:03 -0600

    second commit

--- LargestPalindromeProduct/src/Main.java
+++ LargestPalindromeProduct/src/Main.java
@@ -1,8 +1,24 @@
 public class Main {
 
     public static void main(String[] args) {
-	    // Palindromic number reads the same both ways
-        // largest two 2-digit number is 9009 = 91 * 99
-        // Find the largest two 3-digit numbers
+	     /*
+	     Palindromic number reads the same both ways
+         largest two 2-digit number is 9009 = 91 * 99
+         Find the largest two 3-digit numbers
+
+         Create a loop that multiplies two numbers together
+         If it's a 5-digit number, check that the numbers in index 0 and 4 are the same and index
+         1 and 3 are the same.
+         If it's a 6-digit number, check index 0/5, 1/4, and 2/3 are the same.
+
+         Use an array for the number.
+	      */
+        int[] palindromicNumber = new int[i];
+
+        for(int i = 100; i <= 999; i++) {
+            System.out.println(i * 999);
+        }
+
+
     }
 }
